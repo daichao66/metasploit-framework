9c7c1cb64f9fba69b8709dee139b9a784cbba9be
William Vu <William_Vu@rapid7.com>
2019-04-26 13:17:13 -0500

    Rearrange module trait output in ReadableText

--- lib/msf/core/module.rb
+++ lib/msf/core/module.rb
@@ -39,9 +39,9 @@ class Module
   autoload :Type, 'msf/core/module/type'
   autoload :UI, 'msf/core/module/ui'
   autoload :UUID, 'msf/core/module/uuid'
-  autoload :SideEffects, 'msf/core/module/side_effects'
   autoload :Stability, 'msf/core/module/stability'
   autoload :Reliability, 'msf/core/module/reliability'
+  autoload :SideEffects, 'msf/core/module/side_effects'
 
   include Msf::Module::Arch
   include Msf::Module::Auth
@@ -59,9 +59,9 @@ class Module
   include Msf::Module::Type
   include Msf::Module::UI
   include Msf::Module::UUID
-  include Msf::Module::SideEffects
   include Msf::Module::Stability
   include Msf::Module::Reliability
+  include Msf::Module::SideEffects
 
   # The key where a comma-separated list of Ruby module names will live in the
   # datastore, consumed by #replicant to allow clean override of MSF module methods.
